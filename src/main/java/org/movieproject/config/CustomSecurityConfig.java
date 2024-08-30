25bea02a1aef849a831cc778e2a85c2305de3d14
archiiive <githubarchive10@gmail.com>
2024-08-30 16:39:01 +0900

    123

--- src/main/java/org/movieproject/config/CustomSecurityConfig.java
+++ src/main/java/org/movieproject/config/CustomSecurityConfig.java
@@ -156,7 +156,7 @@ public class CustomSecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         // 허용할 도메인만 지정
         configuration.setAllowedOrigins(List.of(
-                "http://ec2-43-202-199-97.ap-northeast-2.compute.amazonaws.com:8080",
+                "http://movie-front-env-1.eba-86ekffjf.ap-northeast-2.elasticbeanstalk.com:8080",
                 "http://movie-front-env-1.eba-86ekffjf.ap-northeast-2.elasticbeanstalk.com"
         ));
 
